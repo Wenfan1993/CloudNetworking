a4b6b6e4074ce70c00d2828fc70b3e23958964f0
Wenfan1993 <54221190+Wenfan1993@users.noreply.github.com>
2024-01-20 20:24:09 -0600

    Add files via upload

---
+++ server.py
@@ -0,0 +1,23 @@
+from flask import Flask, request
+import subprocess
+import os
+import socket
+app = Flask(__name__)
+
+seed = 0
+
+@app.get("/")
+def get_seed():
+    return socket.gethostname(), 200
+
+
+@app.post("/")
+def update_seed():
+    subprocess.run(["python", os.path.join(
+            os.getcwd(),'stress_cpu.py'
+        )]
+    )
+    return '', 201
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
